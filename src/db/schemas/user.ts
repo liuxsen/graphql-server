98175c6679623680e2687aaad85bbd5988c09898
刘建辉 <jianhui@yzyw-inc.com>
2019-01-20 22:02:02 +0800

    删除transform

--- src/db/schemas/user.ts
+++ src/db/schemas/user.ts
@@ -1,21 +1,8 @@
-// const mongoose = import
-//   Schema = mongoose.Schema;
-
+//  用户模型
 import { Schema } from '../connect';
 
-const userSchema = new Schema({
-  id: { type: Schema.Types.ObjectId },
+export const UserSchema = new Schema({
   name: { type: String }, //姓名
   pwd: { type: String }, //密码
   age: { type: Number }, //年龄
 });
-
-userSchema.method('transform', function() {
-  var obj = this.toObject();
-  //Rename fields
-  obj.id = obj._id;
-  delete obj._id;
-  return obj;
-});
-
-export const UserSchema = userSchema;
